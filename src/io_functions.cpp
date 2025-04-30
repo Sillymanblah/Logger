c00ff59fe487fbf2feba76223da38adee7983858
John Grzegorczyk <jtgrzeg@gmail.com>
2025-04-30 15:18:23 -0500

    Direction and leading zero fix

--- src/io_functions.cpp
+++ src/io_functions.cpp
@@ -2,6 +2,21 @@
 
 void get_binary_cstring( char* buffer, size_t size, const uint64_t& value )
 {
-	for ( size_t index = 0; index < size; ++index )
-		buffer[ index ] = ( ( value >> index ) & 1ULL ) ? '1' : '0';
+	bool ignore_zero = true; // Ignore leading zeros.
+
+	size_t index = 0; // Index to write to the buffer.
+
+	for ( size_t bit_shift = 0; bit_shift < size; ++bit_shift )
+	{
+		bool bit = ( ( value >> ( size - 1 - bit_shift ) ) & 1ULL ); // Get the bit at the current index.
+		if ( bit )
+		{
+			buffer[ index++ ] = '1';
+			ignore_zero = false;
+		}
+		else if ( !ignore_zero )
+		{
+			buffer[ index++ ] = '0';
+		}
+	}
 }
