be4d8bd0a7ff0ed0b4fc3968fa169f16e8566f1f
John Grzegorczyk <jtgrzeg@gmail.com>
2025-05-06 14:52:27 -0500

    Expanding the `basic_logger` test and adding it to CMakeLists.txt

--- src/tests/basic_logger.cpp
+++ src/tests/basic_logger.cpp
@@ -1,8 +1,19 @@
-#include "../logging.hpp"
+#include "../logstream.hpp"
+#include <iostream>
+#include <ostream>
+
+void test_logger_crash()
+{
+	logstream logger( "test_two.log", logger_base::level::ALL_LEVELS, logger_base::setting::PRINT_CRASH );
+
+	logger << logger_base::level::INFO << "Testing if the logger can recognize a crash!";
+
+	throw std::runtime_error( "This is a test exception!" );
+}
 
 int main()
 {
-	logstream logger( "test.txt", logger_base::level::ALL_LEVELS, logger_base::setting::NONE_SET );
+	logstream logger( "test.log", logger_base::level::ALL_LEVELS, logger_base::setting::NONE_SET );
 
 	logger << logger_base::level::DEBUG << "This is a debug print!";
 	logger << logger_base::level::TRACE << "This is a trace print!";
@@ -10,4 +21,7 @@ int main()
 	logger << logger_base::level::WARN << "This is a warning print!";
 	logger << logger_base::level::ERROR << "This is an error print!";
 	logger << logger_base::level::FATAL << "This is a fatal error print!";
+
+	try { test_logger_crash(); }
+	catch ( std::exception& e ) { std::cerr << "Crashed logger with exception:\n\t" << e.what() << std::endl; }
 }
