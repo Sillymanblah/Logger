91e3bb7c08eb06aebb5ac4ad065298b654a7247a
John Grzegorczyk <jtgrzeg@gmail.com>
2025-08-10 02:44:29 -0500

    Refining log_base and adding functionality.

--- src/log_base.cpp
+++ src/log_base.cpp
@@ -1,16 +1,71 @@
 #include "log_base.hpp"
 #include "logstream.hpp"
+#include "time_fix.hpp"
 
-logstream log_base::my_log;
+logstream log_base::my_log = /* Add construction here */;
 
 log_settings log_base::update_settings( log_settings new_settings )
-{ this->settings = new_settings; }
+{
+	// Get the previous settings.
+	log_settings previous = this->settings;
+
+	// Update the settings.
+	this->settings = new_settings;
+	
+	// Return the old settings.
+	return previous;
+}
 
 log_settings log_base::enable_settings( log_settings new_settings )
-{ this->settings.enable( new_settings ); }
+{
+	// Get the previous settings.
+	log_settings previous = this->settings;
+
+	// Update the settings.
+	this->settings.enable( new_settings );
+	
+	// Return the old settings.
+	return previous;
+}
 
 log_settings log_base::toggle_settings( log_settings new_settings )
-{ this->settings.toggle( new_settings ); }
+{
+	// Get the previous settings.
+	log_settings previous = this->settings;
+
+	// Update the settings.
+	this->settings.toggle( new_settings );
+	
+	// Return the old settings.
+	return previous;
+}
 
 log_settings log_base::disable_settings( log_settings new_settings )
-{ this->settings.disable( new_settings ); }
+{
+	// Get the previous settings.
+	log_settings previous = this->settings;
+
+	// Update the settings.
+	this->settings.disable( new_settings );
+	
+	// Return the old settings.
+	return previous;
+}
+
+bool log_base::is_logged( logging_level level )
+{ return this->settings & level; }
+
+std::tm* log_base::log_start()
+{
+	// Get the current time.
+	this->last_time = std::time( NULL );
+	
+	// Convert it into the proper time format.
+	localtime_s( &this->last_time_data, &this->last_time );
+}
+
+log_settings log_base::settings()
+{ return this->settings; }
+
+std::time_t log_base::last_log()
+{ return this->last_time; }
--- src/log_base.hpp
+++ src/log_base.hpp
@@ -14,21 +14,13 @@
 /// @brief Base class of any logstream, which stores a logger to track log data about the logs themselves when running with `LOGGING_DEBUG` set to true.
 class log_base
 {
-protected:
-	/// @brief The settings associated with this logstream
-	log_settings settings;
-
-	/// @brief The start time of the last log, used to store the time temporarily for `start_log` and `std::format`.
-	std::time_t last_time;
-
-	/// @brief The time struct for the last log's start time, also used for the storage duration to push data to the stream.
-	/// The primary difference is this member is not offered by a getter, one must create their own `std::tm` object if they need one.
-	std::tm last_time_data;
-
-	/// @brief The log that all logs use for debugging their lifetime.
-	static logstream my_log;
-
 public:
+	log_base() = default;
+	log_base( const log_base& ) = default;
+	log_base( log_base&& ) = default;
+	log_base& operator = ( const log_base& ) = default;
+	log_base& operator = ( log_base&& ) = default;
+
 	/// @brief Updates the @ref log_settings for this logger.
 	///
 	/// @param new_settings The new `log_settings`.
@@ -52,6 +44,34 @@ public:
 	/// @param new_settings The new `log_settings`.
 	/// @return The previous `log_settings` that was replaced.
 	log_settings disable_settings( log_settings new_settings );
+
+protected:
+	bool is_logged( logging_level level );
+	std::tm* log_start();
+
+public:
+	log_settings settings();
+	std::time_t last_log();
+
+private:
+	/// @brief The settings associated with this logstream
+	log_settings settings;
+
+protected:
+	/// @brief The start time of the last log, used to store the time temporarily for `start_log` and `std::format`.
+	std::time_t last_time;
+
+	/// @brief The time struct for the last log's start time, also used for the storage duration to push data to the stream.
+	/// The primary difference is this member is not offered by a getter, one must create their own `std::tm` object if they need one.
+	std::tm last_time_data;
+
+/// @section Static
+/// @{
+public:
+	/// @brief The log that all logs use for debugging their lifetime.
+	static logstream my_log;
+
+/// @}
 };
 
 #endif // LOG_BASE_HPP
