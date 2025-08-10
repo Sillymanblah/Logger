f45d0f8e086e303665a610a3b68e542d7debbe50
John Grzegorczyk <jtgrzeg@gmail.com>
2025-08-10 18:31:16 -0500

    Fixing the return type.

--- src/log_stream.hpp
+++ src/log_stream.hpp
@@ -131,7 +131,7 @@ public:
 	/// 
 	/// @param level The `logging_level` of this log.
 	/// @return A `basic_logstream_log` for outputting the rest of the data.
-	log&& operator << ( logging_level level )
+	log operator << ( logging_level level )
 	{
 		// If we are not logging this data currently, create a log consumer.
 		if ( !this->is_logged( level ) ) return log( nullptr );
@@ -143,7 +143,7 @@ public:
 		this->start_log( new_log, level );
 
 		// Return the log we created for the user to do output operations.
-		return reinterpret_cast< log&& >( new_log );
+		return std::move( new_log );
 	}
 
 	/// @brief Update the current time format with a new one.
